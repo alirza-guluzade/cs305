4a21a0d04f4e9e509f4904d2113fc787b452e90d
alirzatg <alirzatg@code.edu.az>
2024-09-23 19:19:01 +0400

    task1

---
+++ Homework/Task 1/task1.js
@@ -0,0 +1,64 @@
+// Programming Task 1
+// Student : Alirza Guluzade   Class : CS305
+
+// Data types : 
+// 1. create a string , number , boolean , undefined , null , symbol , and biginit variable .
+// 2. print each variable type using typeof.
+
+let x = "Code academy "
+console.log(typeof x)   //String
+
+let y = 56
+console.log(typeof y)   // Number
+
+let z = true
+console.log(typeof z)  // Boolean
+
+let user;
+console.log(typeof user); //Undefined
+
+let c = null 
+console.log(typeof c) //object
+
+let b = Symbol("Alirza")
+console.log(typeof b)  // symbol
+
+let a = 122222222234333422n
+console.log(typeof a) //biginit
+
+// Keywords :
+// 3.use let,const, and var to declare variables and explain  their differences with examples.
+// backticks (template literials) :
+// 4. use backticks to create a dynamic message that includes (firstname,lastname,age,country,hobby,occupation) variables.
+
+const firstname = 'Alirza'
+const lastname = 'Guluzade'
+let age = 18
+let country = 'Azerbaijan'
+let hobby = 'football'
+let occupation = 'Cyber Security specialist'
+
+console.log(`My fullname is ${firstname} ${lastname}.I am ${age} years old and I am from ${country}.My hobby is ${hobby}. My job is ${occupation}`)
+
+//operators :
+// 5. Use arithmetic operators (+ , - , * , /, %).
+// 6. Use comparison operators (== , ===, > , < , etc.)
+// 7. use logıcal operators (&& , | | , !)
+
+console.log("1" + 1 + 9 ) // + operator
+console.log(1+4)
+console.log(4+7+'1'+ 2)
+
+console.log(5-3) // - operator
+console.log(10 / 5) // / operator
+console.log(10 * 5) // * operator
+console.log(20%9) // % operator
+
+
+
+
+
+
+
+
+
