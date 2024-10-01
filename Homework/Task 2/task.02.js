a786de55be530ed01f15a480b4f4865292c72d52
alirzatg <alirzatg@code.edu.az>
2024-10-01 10:18:11 +0400

    task2

--- Homework/Task 2/task.02.js
+++ Homework/Task 2/task.02.js
@@ -3,10 +3,128 @@
 // Student : Alirza Guluzade  //
 // // // // // // // // // // //
 
-// # Mission.01 #
+// ###### Task 1 ######
 
-// ~ Exercise.01 ~
-// Multiply 12 with 8 , and console the result:
+// 1.Multiply 12 with 8, and console the result:
+
+
+const a = 12 ;
+const b = 8  ;
+const c = a * b 
+
+console.log(c)
+
+///////////////////
+
+console.log(12 * 8)
+
+
+// 2.Divide 10 by 2 add result to variables x, and console x:
+
+
+let h = 10
+let p = 2
+let x = h / p
+
+console.log(x)
+
+/* 
+
+let x = 10 / 2
+console.log(x)
+
+*/
+
+
+// 3.Create two variables called num1 and num2, assign the value 20 to num1 and value 17 to num2 then addition(+) them in console.
+
+
+const num1 = 20
+const num2 = 17
+console.log(num1 + num2)
+
+
+/*
+4.Create variables called name, surname and year assign values to them ('your name', 'your surname', ' your birth of year'),
+then add them to varibale name (info) and console info. 
+*/
+  
+
+let name = 'Alirza'
+let surname = 'Guluzade'
+let year = 2006
+
+let info = name + '  ' + surname + '  ' + year
+
+console.log(info)
+
+
+// 5.Console the remainder(%) when 17 is divided by 12.
+
+
+var y = 17 % 12
+console.log(y);
+
+/*
+6.Create a varibale called "city name" (camelCase). Assign value Jalilabad, then call variable again
+and reassign value Baku, console result.
+*/
+
+let cityName ;
+
+cityName = ' Jalilabad'
+
+cityName = 'Baku '
+
+console.log(cityName)
+
+
+// ###### Task 2 ######
+
+// console.log('test1', test); // result 1 = test is not defined. 
+
+//{let test = "something"
+//console.log('test2', test); }
+
+//console.log('test3', test);  // result 3
+
+
+//console.log('test1', test);  // ?
+// {
+//     var test = "something"
+//     console.log('test2', test); // ?
+// }
+// console.log('test3', test); // ?
+
+
+// ###### Task 3 ######
+
+//Find the type of all the following cases
+
+let names = "Ulfat"
+// let surnames = Zakirli - is not defined
+let years = 2000
+years = "2000"
+let city;
+let qualification = null
+let obj = { name: 'ulfat' }
+let arr = ['a', 'b', 'c']
+
+console.log('names' , typeof names);
+//console.log('surnames' ,typeof surnames); - is not defined
+console.log('years',typeof years);
+console.log('city',typeof city);
+console.log('qualification' ,typeof qualification);
+console.log('obj' ,typeof obj);
+console.log('arr' ,typeof arr);
+
+
+
+
+
+
+
+
 
 
 
