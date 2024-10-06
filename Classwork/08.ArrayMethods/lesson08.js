48416fa94ce4ef17e80e4aba8ff8e114dcb9af0f
alirzatg <alirzatg@code.edu.az>
2024-10-06 14:41:01 +0400

    task2

---
+++ Classwork/08.ArrayMethods/lesson08.js
@@ -0,0 +1,68 @@
+const numbers = [9 ,5 , 3 ,11 , 12 , 33 , 22 , 9 , 11 , 10 , 0 ,8]
+
+// 1.reverse 'numbers'
+
+// const number1 = numbers.reverse()
+// console.log(number1)
+
+
+
+// 2. sort asc
+
+// numbers.sort((a,b) => a-b )
+// console.log(numbers)
+
+
+
+
+//3.
+// numbers.sort((a,b) => b-a )
+// console.log(numbers)
+
+
+
+
+//4.
+// const result = numbers.splice(3,4)
+// console.log(numbers)
+// console.log(result)
+
+
+
+// //5.
+// const result2 = numbers.findIndex((item ,index) => item === 11)
+// console.log(result2);
+
+
+
+//6
+// const result3 = numbers.sort((a,b) => a -b).filter((item ,index) => item > 10)
+// console.log(result3);
+
+
+
+
+//7
+// numbers.push(66 , 77)
+// console.log(numbers);
+
+
+
+//8
+
+
+
+//9
+// numbers.shift()
+// numbers.shift()
+// console.log(numbers);
+
+
+
+//10
+// numbers.unshift(0,1,2)
+// console.log(numbers);
+
+
+
+
